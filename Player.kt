a1ad8bc8cd0a0df3c2c270880a71cceb41c5b588
JvstvsHD <79066214+JvstvsHD@users.noreply.github.com>
2021-08-30 18:35:31 +0200

    Delete Player.kt

--- Player.kt
+++
@@ -1,8 +0,0 @@
-package chess
-
-enum class Player(val playerName: String, val char: Char) {
-    WHITE("White", 'w'),
-    BLACK("Black", 'b');
-
-    fun opposite(): Player = if (this == WHITE) BLACK else WHITE
-}
